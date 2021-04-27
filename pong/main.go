4297e0e6ee6c821e121613b099f80e1433f5affb
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2021-04-27 23:15:40 +0200

    feat(pong): add seq property

--- pong/main.go
+++ pong/main.go
@@ -4,13 +4,18 @@ import "time"
 
 // Pong is payload sent to client
 type Pong struct {
+	Seq         int
 	CurrentTime time.Time
 	Message     string
 }
 
+var seq int
+
 // NewPong creates a new Pong
 func NewPong(msg string) Pong {
+	seq += 1
 	return Pong{
+		Seq:         seq,
 		CurrentTime: time.Now().UTC(),
 		Message:     msg,
 	}
