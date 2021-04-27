ccebefcf7429905eb8cb5337a962d5dad6baedd6
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2021-04-27 23:16:23 +0200

    feat(ping): add channel generating pongs

--- protos/main.go
+++ protos/main.go
@@ -2,6 +2,7 @@ package ping
 
 import (
 	"encoding/json"
+	"math/rand"
 	"time"
 
 	"github.com/tengla/grpc-ping/pong"
@@ -15,20 +16,42 @@ func NewPingServer() *pingServer {
 	return &pingServer{}
 }
 
+func pingChan() chan interface{} {
+	ch := make(chan interface{})
+	go func() {
+		defer func() {
+			close(ch)
+		}()
+		messages := []string{"ABC", "DEF", "GHI", "JKL", "MNO", "PQR", "STU", "VWX", "YZ"}
+		for {
+			message := messages[int(rand.Int()%len(messages))]
+			data, err := json.Marshal(
+				pong.NewPong(message))
+			if err != nil {
+				ch <- err
+			}
+			r := &PingResponse{
+				Payload: string(data),
+			}
+			ch <- r
+			time.Sleep(time.Second * time.Duration(rand.Float32()*4))
+		}
+	}()
+	return ch
+}
+
+// Ping - grpc stream
 func (p *pingServer) Ping(req *Empty, stream PingService_PingServer) error {
-	for {
-		pong := pong.NewPong("Keep me alive")
-		data, err := json.Marshal(pong)
-		if err != nil {
-			return err
-		}
-		r := &PingResponse{
-			Payload: string(data),
-		}
-		time.Sleep(time.Second)
-		err = stream.Send(r)
-		if err != nil {
-			return err
+	for v := range pingChan() {
+		switch t := v.(type) {
+		case *PingResponse:
+			err := stream.Send(t)
+			if err != nil {
+				return err
+			}
+		case error:
+			return t
 		}
 	}
+	return nil
 }
